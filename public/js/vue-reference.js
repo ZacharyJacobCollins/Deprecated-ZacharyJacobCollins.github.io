1b05c69e4e6e5f423d2caf56cbb990341ce21224
Zack Collins <Zack Collins>
2016-09-11 21:48:22 -0400

    not a productive day at all

---
+++ public/js/vue-reference.js
@@ -0,0 +1,17 @@
+//The index/title page
+var index = new Vue({
+  el: "#referenceIndex",
+  data: {
+    subjects:  [
+      { title: "Go" },
+      { title: "Laravel" },
+      { title: "Kubernetes" },
+      { title: "Docker" },
+      { title: "Tensorflow" },
+      { title: "Cracking the Coding Interview" },
+      { title: "Earning" },
+      { title: "Podcasts" },
+      { title: "Music" },
+    ]
+  }
+});
