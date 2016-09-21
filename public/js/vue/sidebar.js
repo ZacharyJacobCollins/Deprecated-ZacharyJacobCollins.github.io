6ac2e66f0977e1b30fc8094ce562eada35914b6a
Zac Collins <Zac Collins>
2016-09-21 19:18:04 -0400

    vue updates

--- public/js/vue/sidebar.js
+++ public/js/vue/sidebar.js
@@ -1,20 +1,51 @@
-new Vue({
+var sidebar = new Vue({
   el: '#sidebar',
   data: {
-        college: [
-          { text: 'Statistics 360', link: '/stats'}
-        ],
-        projects: [
+    sections: [
+        {
+          title: 'College',
+          content: [
+            { text: 'Statistics 360', link: '/stats'},
+            { text: 'Discrete Math', link: '/discrete'},
+          ]
+        },
+        {
+          title: 'Projects',
+          content: [
 
-        ],
-        bodybuilding: [
+          ]
+        },
+        {
+          title: 'Body Building',
+          content: [
 
-        ],
-        snippets: [
+          ]
+        },
+        {
+          title: 'Snippets',
+          content: [
 
-        ],
-        books: [
+          ]
+        },
+        {
+          title: 'Books',
+          content: [
 
+          ],
+        },
+      ],
+    }
+});
+
+var topNav = new Vue({
+  el: '#topNav',
+  data: {
+        buttons: [
+          { text: 'Home', link: '/home'},
+          { text: 'Projects', link: '/projects'},
+          { text: 'Body Building', link: '/bodybuilding'},
+          { text: 'Snippets', link: '/snippets'},
+          { text: 'Books', link: '/books'},
         ],
       },
-})
+});
