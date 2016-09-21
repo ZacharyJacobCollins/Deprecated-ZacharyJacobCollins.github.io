c3cbe228e4770caec67b545dcec9af4afbb2216b
Zac Collins <Zac Collins>
2016-09-20 20:28:05 -0400

    added in vue.js nav bar

---
+++ public/js/vue/sidebar.js
@@ -0,0 +1,20 @@
+new Vue({
+  el: '#sidebar',
+  data: {
+        college: [
+          { text: 'Statistics 360', link: '/stats'}
+        ],
+        projects: [
+
+        ],
+        bodybuilding: [
+
+        ],
+        snippets: [
+
+        ],
+        books: [
+
+        ],
+      },
+})
