4562a7299d29888e4564d3c2bc600a1e5beeef88
Zac Collins <Zac Collins>
2016-09-28 11:20:10 -0400

    Updates to the projects including...

---
+++ public/js/vue/stats.js
@@ -0,0 +1,64 @@
+var stats = new Vue({
+    el: '#stats',
+    data: {
+      Chapters: [
+        {
+          title: 'Chapter 4',
+          definitions: [
+            { term: 'Remember', definition: 'test'},
+            { term: 'Remember', definition: 'test'},
+            { term: 'Remember', definition: 'test'},
+            { term: 'Remember', definition: 'test'},
+            { term: 'Remember', definition: 'test'},
+            { term: 'Remember', definition: 'test'},
+            { term: 'Remember', definition: 'test'},
+            { term: 'Remember', definition: 'test'},
+            { term: 'Remember', definition: 'test'},
+            { term: 'Remember', definition: 'test'},
+            { term: 'Remember', definition: 'test'},
+            { term: 'Remember', definition: 'test'},
+            { term: 'Remember', definition: 'test'},
+            { term: 'Remember', definition: 'test'},
+          ],
+        },
+        {
+          title: 'Chapter 5',
+          definitions: [
+            { term: 'Remember', definition: 'test'},
+            { term: 'Remember', definition: 'test'},
+            { term: 'Remember', definition: 'test'},
+            { term: 'Remember', definition: 'test'},
+            { term: 'Remember', definition: 'test'},
+            { term: 'Remember', definition: 'test'},
+            { term: 'Remember', definition: 'test'},
+            { term: 'Remember', definition: 'test'},
+            { term: 'Remember', definition: 'test'},
+            { term: 'Remember', definition: 'test'},
+            { term: 'Remember', definition: 'test'},
+            { term: 'Remember', definition: 'test'},
+            { term: 'Remember', definition: 'test'},
+            { term: 'Remember', definition: 'test'},
+          ],
+        },
+        {
+          title: 'Chapter 6',
+          definitions: [
+            { term: 'Remember', definition: 'test'},
+            { term: 'Remember', definition: 'test'},
+            { term: 'Remember', definition: 'test'},
+            { term: 'Remember', definition: 'test'},
+            { term: 'Remember', definition: 'test'},
+            { term: 'Remember', definition: 'test'},
+            { term: 'Remember', definition: 'test'},
+            { term: 'Remember', definition: 'test'},
+            { term: 'Remember', definition: 'test'},
+            { term: 'Remember', definition: 'test'},
+            { term: 'Remember', definition: 'test'},
+            { term: 'Remember', definition: 'test'},
+            { term: 'Remember', definition: 'test'},
+            { term: 'Remember', definition: 'test'},
+          ],
+        },
+      ]
+    }
+});
