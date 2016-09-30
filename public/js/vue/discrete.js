5459820fd12bbcf263d9cb3dba508746147367eb
Zac Collins <Zac Collins>
2016-09-30 00:09:43 -0400

    Updates to personal project using alias

--- public/js/vue/discrete.js
+++ public/js/vue/discrete.js
@@ -10,7 +10,6 @@ var discrete = new Vue({
             { title: 'Warshal\'s Algorithm', text: ' '},
             { title: 'Practice Problems\'s', text: 'Pg 606 #1,3,5,19,25,27,29'},
             { title: 'Lab 1', text: '2^n is number of subsets'},
-
           ],
         },
       ]
