82a95027ac25e4031a008c9c378a65d784eecfe5
Zac Collins <Zac Collins>
2016-10-02 12:21:31 -0400

    Updates to personal project using alias

--- public/js/vue/components.js
+++ public/js/vue/components.js
@@ -1,5 +1,5 @@
 //HEADER COMPONENT
-var head = Vue.extend({
+var headTemplate = Vue.extend({
   template: `
     <head>
       <meta charset="utf-8">
@@ -11,17 +11,11 @@ var head = Vue.extend({
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <link href='https://fonts.googleapis.com/css?family=Miriam+Libre:400,700|Source+Sans+Pro:200,400,700,600,400italic,700italic' rel='stylesheet' type='text/css'>
       <!--[if lte IE 9]>
-        <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+      <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
       <![endif]-->
       <link rel="stylesheet" href="/public/css/laravel-9d78469b5d.css">
       <link rel="apple-touch-icon" href="/favicon.png">
 
-      <!-- Larger Vue Components -->
-      <script src="/public/js/vue/navs.js"></script>
-      <script src="/public/js/vue/topnav.js"></script>
-      <script src="/public/js/vue/footer.js"></script>
-
-      
     </head>
   `
 });
@@ -43,16 +37,8 @@ var foot = Vue.extend({
   `
 });
 
-var scripts = Vue.extend({
-  template: `
-    <!-- Larger Vue Components -->
-    <script src="/public/js/vue/navs.js"></script>
-    <script src="/public/js/vue/topnav.js"></script>
-    <script src="/public/js/vue/footer.js"></script>
-  `
-});
-
-var sidenav = Vue.extend({
+var sidenavTemplate = Vue.extend({
+  props: ['sections'],
   template: `
   <section id="sideNav" class="sidebar">
     <ul>
@@ -69,12 +55,10 @@ var sidenav = Vue.extend({
   </section>`
 });
 
-
 // register the components glbally to vue.js
-Vue.component('sidenav-component', sidenav);
-Vue.component('header-component', head);
-Vue.component('footer-component', foot);
-Vue.component('scripts-component', scripts);
+Vue.component('sidenav-component', sidenavTemplate);
+Vue.component('header-component', headTemplate);
+Vue.component('footer-component', footTemplate);
 
 
 // create a root vue instance
@@ -84,17 +68,3 @@ new Vue({
     console.log('Stylesheet and footer components loaded');
   }
 });
-
-// <section id="sideNav" class="sidebar">
-//   <ul>
-//     <template v-for="section in sections">
-//       <li><strong>{{ section.title }}</strong>
-//         <ul>
-//           <template v-for="c in section.content">
-//               <li><a href="{{ c.link }}">{{ c.text }}</a></li>
-//           </template>
-//         </ul>
-//       </li>
-//     </template>
-//   </ul>
-// </section>
