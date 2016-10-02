9f251c6491687140038fa033e84eb4235838d7f2
Zac Collins <Zac Collins>
2016-10-02 15:27:02 -0400

    Updates to personal project using alias

--- public/js/vue/components.js
+++ public/js/vue/components.js
@@ -1,27 +1,4 @@
-//HEADER COMPONENT
-var headTemplate = Vue.extend({
-  template: `
-    <head>
-      <meta charset="utf-8">
-      <title>Zachary Jacob Collins</title>
-      <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-      <meta name="author" content="Zachary Collins">
-
-    <meta name="keywords" content="zac, zac collins, zachary collins, zachary jacob collins, computer science, web, blog, tech, google">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <link href='https://fonts.googleapis.com/css?family=Miriam+Libre:400,700|Source+Sans+Pro:200,400,700,600,400italic,700italic' rel='stylesheet' type='text/css'>
-      <!--[if lte IE 9]>
-      <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-      <![endif]-->
-      <link rel="stylesheet" href="/public/css/laravel-9d78469b5d.css">
-      <link rel="apple-touch-icon" href="/favicon.png">
-
-    </head>
-  `
-});
-
-//FOOTER COMPONENT
-var footTemplate = Vue.extend({
+Vue.component('footer-component', {
   template: `
   <footer id="footer" class="main">
       <ul>
@@ -37,15 +14,40 @@ var footTemplate = Vue.extend({
   `
 });
 
-var sidenavTemplate = Vue.extend({
-  props: ['sections'],
+// register the components glbally to vue.js
+Vue.component('header-component', {
+  template: `
+      <head>
+        <meta charset="utf-8">
+        <title>Zachary Jacob Collins</title>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta name="author" content="Zachary Collins">
+
+      <meta name="keywords" content="zac, zac collins, zachary collins, zachary jacob collins, computer science, web, blog, tech, google">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href='https://fonts.googleapis.com/css?family=Miriam+Libre:400,700|Source+Sans+Pro:200,400,700,600,400italic,700italic' rel='stylesheet' type='text/css'>
+        <!--[if lte IE 9]>
+        <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+        <link rel="stylesheet" href="public/css/laravel-9d78469b5d.css">
+        <link rel="apple-touch-icon" href="/favicon.png">
+
+      </head>`,
+});
+
+
+Vue.component('sidenav-component', {
+  // declare the props
+  props: ['sideNavSections'],
+  // the prop can be used inside templates, and will also
+  // be set as `this.msg`
   template: `
   <section id="sideNav" class="sidebar">
     <ul>
-      <template v-for="section in sections">
-        <li><strong>{{ section.title }}</strong>
+      <template v-for="sideNavSection in sideNavSections">
+        <li><strong>{{ sideNavSection.title }}</strong>
           <ul>
-            <template v-for="c in section.content">
+            <template v-for="c in sideNavSection.content">
                 <li><a href="{{ c.link }}">{{ c.text }}</a></li>
             </template>
           </ul>
@@ -54,11 +56,6 @@ var sidenavTemplate = Vue.extend({
     </ul>
   </section>`
 });
-
-// register the components glbally to vue.js
-Vue.component('sidenav-component', sidenavTemplate);
-Vue.component('header-component', headTemplate);
-Vue.component('footer-component', footTemplate);
 
 
 // create a root vue instance
