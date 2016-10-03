cef38c7dfa5cf3f735e26115da27468ae05cb665
Zac Collins <zcollins@sscorp.com>
2016-10-03 15:39:06 -0400

    Updates to personal project using alias

--- public/js/vue/components.js
+++ public/js/vue/components.js
@@ -13,6 +13,7 @@ Vue.component('header-component', {
         <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
         <![endif]-->
         <link rel="stylesheet" href="/public/css/laravel-9d78469b5d.css">
+        <link rel="stylesheet" href="/public/css/style.css">
         <link rel="apple-touch-icon" href="/favicon.png">
       </head>`,
 });
