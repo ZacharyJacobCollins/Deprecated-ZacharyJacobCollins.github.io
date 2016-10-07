869ddd46fe95dbe00f469cd646d5ff36b03704eb
Zac Collins <zcollins@sscorp.com>
2016-10-07 09:46:36 -0400

    Updates to personal project using alias

--- public/js/vue/components/technology/sql-component.js
+++ public/js/vue/components/technology/sql-component.js
@@ -13,14 +13,16 @@ Vue.component('sql-component', {
   `,
   data: function() {
     return {
-      concepts: [
-          title: 'Data Types',
-          individualConcepts: [
-              { term: 'char', definition: 'Maximum length of 8,000 characters.( Fixed length non-Unicode characters)' },
-              { term: 'varchar', definition: 'Maximum of 8,000 characters.(Variable-length non-Unicode data).' },
-              { term: 'varchar(max)', definition: 'Maximum length of 231characters, Variable-length non-Unicode data (SQL Server 2005 only).' },
-              { term: '', definition: '' },
-          ],
+        concepts: [
+          {
+              title: 'Devops',
+              individualConcepts: [
+                  { term: 'char', definition: 'Maximum length of 8,000 characters.( Fixed length non-Unicode characters)' },
+                  { term: 'varchar', definition: 'Maximum of 8,000 characters.(Variable-length non-Unicode data).' },
+                  { term: 'varchar(max)', definition: 'Maximum length of 231characters, Variable-length non-Unicode data (SQL Server 2005 only).' },
+                  { term: '', definition: '' },
+              ],
+          },
         ],
       },
     }
