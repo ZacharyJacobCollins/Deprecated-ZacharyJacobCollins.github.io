419683edfe8382e0f2e83aa1f214302bef8e0c17
Zac Collins <zcollins@sscorp.com>
2016-10-07 09:16:15 -0400

    Updates to personal project using alias

--- public/js/vue/components/technology/sql-component.js
+++ public/js/vue/components/technology/sql-component.js
@@ -1,6 +1,27 @@
 Vue.component('sql-component', {
   template:
   `
-    <h1>SQL</h1>
+    <h2>SQL</h2>
+    <template v-for="concept in concepts">
+      <h2>{{ concept.title }}</h2>
+      <ul>
+          <template v-for="individual in concept.individualConcepts">
+              <li><strong>{{ individual.term }}</strong>: {{ individual.definition }}</li>
+          </template>
+      </ul>
+    </template>
   `,
+  data: function() {
+    return {
+      {
+          title: 'Data Types',
+          individualConcepts: [
+              { term: 'char', definition: 'Maximum length of 8,000 characters.( Fixed length non-Unicode characters)' },
+              { term: 'varchar', definition: 'Maximum of 8,000 characters.(Variable-length non-Unicode data).' },
+              { term: 'varchar(max)', definition: 'Maximum length of 231characters, Variable-length non-Unicode data (SQL Server 2005 only).' },
+              { term: '', definition: '' },
+          ]
+      },
+    }
+  }
 });
