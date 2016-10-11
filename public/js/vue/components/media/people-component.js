3f982b50635c121ce0a61406ae379312c5ab8573
Zac Collins <Zac Collins>
2016-10-11 16:50:45 -0400

    Updates to personal project using alias

--- public/js/vue/components/media/people-component.js
+++ public/js/vue/components/media/people-component.js
@@ -1,6 +1,37 @@
 Vue.component('people-component', {
   template:
   `
-    <h1> People </h1>
+  <template v-for="person in people">
+    <h2>{{ person.name }}</h2>
+    <ul>
+      <li><strong>{{ person.name }}</strong>: {{ person.relevance }}</li>
+    </ul>
+  </template>
   `,
+  data: function() {
+    return {
+      people: [
+        {
+          name: 'David DeSandro ',
+          link: '',
+          relevance: 'Codepen/Pinterest dude.  Good frontend stuff.  ',
+        },
+        {
+          name: 'Les Twins',
+          link: '',
+          relevance: 'Phenomenal Dancers',
+        },
+        {
+          name: 'Eric Thomas',
+          link: '',
+          relevance: 'Motivational Speaker  ',
+        },
+        {
+          name: 'Haseeb Qureshi',
+          link: '',
+          relevance: 'Works at Air b n b, effective altruist, ex pro poker player, really cool software engineering guy'
+        },
+      ]
+    }
+  }
 });
