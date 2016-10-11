8d38c88eb466cd69da68b1b0e1b183878e380e0d
Zac Collins <Zac Collins>
2016-10-11 17:52:23 -0400

    Updates to personal project using alias

--- public/js/vue/components/media/videos-component.js
+++ public/js/vue/components/media/videos-component.js
@@ -1,8 +1,25 @@
 Vue.component('videos-component', {
   template:
   `
-    <h1> Videos </h1>
-    <h3> Inspirational </h3>
-    <h3> Educational </h3>
+  <template v-for="section in sections">
+    <h2>{{ section.title }}</h2>
+      <ul>
+        <template v-for="video in section.videos">
+            <li><strong>{{ video.title }}</strong>: {{ video.link}}</li>
+        </template>
+      </ul>
+  </template>
   `,
+  data: function() {
+    return {
+      sections: [
+        {
+            title: 'Inspirational',
+            videos: [
+                { title: 'CT Fletcher bodybuilding', link: 'https://www.youtube.com/watch?v=DHTN3oXNDOA' },
+            ],
+        },
+      ]
+    };
+  }
 });
