0bf2b271c53ab847c186b71e26f551a8bee1b6a8
Zac Collins <Zac Collins>
2016-10-11 12:14:52 -0400

    Updates to personal project using alias

--- public/js/vue/components/technology/js-component.js
+++ public/js/vue/components/technology/js-component.js
@@ -2,5 +2,23 @@ Vue.component('js-component', {
   template:
   `
     <h1>Javascript</h1>
+    <p>
+
+$.ajax({
+    type: "POST",
+    url: "/webservices/PodcastService.asmx/CreateMarkers",
+    // The key needs to match your method's input parameter (case-sensitive).
+    data: JSON.stringify({ Markers: markers }),
+    contentType: "application/json; charset=utf-8",
+    dataType: "json",
+    success: function(data){alert(data);},
+    failure: function(errMsg) {
+        alert(errMsg);
+    }
+});
+
+you can import scripts to web workers with
+importScripts('script1.js');
+    </p>
   `,
 });
