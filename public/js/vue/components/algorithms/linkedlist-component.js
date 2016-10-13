8650a6c8105382d486cc981ab17a2d8901822f0c
Zac Collins <zcollins@sscorp.com>
2016-10-13 18:49:23 -0400

    Updates to personal project using alias

--- public/js/vue/components/algorithms/linkedlist-component.js
+++ public/js/vue/components/algorithms/linkedlist-component.js
@@ -5,11 +5,7 @@ Vue.component('linkedlist-component', {
     <a href="https://gist.github.com/ZacharyJacobCollins/c9ccdb2dda2ad3ab65eec65b8ef5367e">Link to gist</a>
     <h3>Notable Features </h3>
     <ul>
-        <li>The number of nodes in a binary tree of depth n is precisely 2^n - 1.</li>
-        <li>In a binary tree, the worst search time is O(n), best is O(log_2 (n)) which is a perfectly balanced tree.</li>
-        <li>Average runtime is somewhere between those two.</li>
-        <li>Left of node is lesser than parent.  Right is greater.</li>
-        <li>To find element that is less than a parent go left, greater than, go right.</li>
+        <li>It's a linked list.</li>
     </ul>
   `,
 });
