cc439cbde38f8220d286b3cf623f753e414230ea
Zac Collins <zcollins@sscorp.com>
2016-10-13 15:28:49 -0400

    Updates to personal project using alias

--- public/js/vue/components/algorithms/linkedlist-component.js
+++ public/js/vue/components/algorithms/linkedlist-component.js
@@ -2,7 +2,7 @@ Vue.component('linkedlist-component', {
   template:
   `
     <h1> Linked List in Java </h1>
-    <a href="https://gist.github.com/ZacharyJacobCollins/c9ccdb2dda2ad3ab65eec65b8ef5367e"></a>
+    <a href="https://gist.github.com/ZacharyJacobCollins/c9ccdb2dda2ad3ab65eec65b8ef5367e">Link to gist</a>
     <br />
     <br />
     <script src="https://gist.github.com/ZacharyJacobCollins/c9ccdb2dda2ad3ab65eec65b8ef5367e.js"></script>
