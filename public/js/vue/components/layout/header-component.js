2f5b97a7d3f23c74b4e080c28f7ae7c2d055f7d9
Zac Collins <Zac Collins>
2016-10-22 21:19:33 -0400

    Updates to personal project using alias

--- public/js/vue/components/layout/header-component.js
+++ public/js/vue/components/layout/header-component.js
@@ -12,7 +12,7 @@ Vue.component('header-component', {
         <!--[if lte IE 9]>
         <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
         <![endif]-->
-        <link rel="stylesheet" href="/public/css/custom/laravel-9d78469b5d.css">
+        <link rel="stylesheet" href="/public/css/custom/documentation.css">
         <link rel="stylesheet" href="/public/css/customstyle.css">
         <link rel="apple-touch-icon" href="/favicon.png">
       </head>`,
