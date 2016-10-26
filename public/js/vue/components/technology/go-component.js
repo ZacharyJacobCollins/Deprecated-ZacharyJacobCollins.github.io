93144ed98e6b2338d4d912a3f35aaa5dcc4951f0
Zac Collins <Zac Collins>
2016-10-26 00:40:55 -0400

    Updates to personal project using alias

--- public/js/vue/components/technology/go-component.js
+++ public/js/vue/components/technology/go-component.js
@@ -2,5 +2,18 @@ Vue.component('go-component', {
   template:
   `
     <h1> Go </h1>
+    <ul>
+      <template v-for="concept in concepts.">
+          <li><strong>{{ concept.term }}</strong>: {{ concept.text }}</li>
+      </template>
+    </ul>
   `,
+  data: function() {
+    return {
+      concepts: [
+        { term: "Naked return", text: "In function when you have a return that has values x = \"String\" and return with just return at end of func" },
+        { term: "", text: "" },
+      ]
+    };
+  }
 });
