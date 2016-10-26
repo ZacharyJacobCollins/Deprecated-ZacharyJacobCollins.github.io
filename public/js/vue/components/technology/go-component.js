d5713cf3825727f78427f2d435667ae36d2b60aa
Zac Collins <Zac Collins>
2016-10-26 00:42:31 -0400

    Updates to personal project using alias

--- public/js/vue/components/technology/go-component.js
+++ public/js/vue/components/technology/go-component.js
@@ -12,6 +12,9 @@ Vue.component('go-component', {
     return {
       concepts: [
         { term: "Naked return", text: "In function when you have a return that has values x = \"String\" and return with just return at end of func" },
+        { term: "var can declare a list of variables: ", text: "var c, python, java bool" },
+        { term: "undeclared variables are initialized to 0 or false etc.  ", text: "Ex.  var python when printed fmt.print(python) comes out 'false'" },
+        { term: "", text: "" },
         { term: "", text: "" },
       ]
     };
