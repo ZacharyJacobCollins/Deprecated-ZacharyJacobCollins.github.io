d0a6cb8461f66d258a0a0664e0213d1e2368cd07
Zac Collins <Zac Collins>
2016-10-27 02:52:21 -0400

    Updates to personal project using alias

--- public/js/vue/components/technology/go-component.js
+++ public/js/vue/components/technology/go-component.js
@@ -15,6 +15,9 @@ Vue.component('go-component', {
         { term: "var can declare a list of variables: ", text: "var c, python, java bool" },
         { term: "undeclared variables are initialized to 0 or false etc.  ", text: "Ex.  var python when printed fmt.print(python) comes out 'false'" },
         { term: "Can initialize with initialize sequence ex.  ", text: "var i, j int = 1, 2" },
+        { term: "Rune", text: "Alias for int32" },
+        { term: "Type Conversions", text: "The expression T(v) converts the value v to the type T." },
+        { term: "Constants", text: "Constants are declared like variables, but with the const keyword. const Pi = 3.14" },
         { term: "", text: "" },
         { term: "", text: "" },
         { term: "", text: "" },
@@ -23,10 +26,7 @@ Vue.component('go-component', {
         { term: "", text: "" },
         { term: "", text: "" },
         { term: "", text: "" },
-        { term: "", text: "" },
-        { term: "", text: "" },
-        { term: "", text: "" },
-        { term: "", text: "" },
+        { term: "Bookmark", text: "https://tour.golang.org/flowcontrol/1" },
       ]
     };
   }
