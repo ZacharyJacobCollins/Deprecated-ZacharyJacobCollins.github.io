5dc34fe31ab04131f72096b33bc289d6e48202ac
Zac Collins <Zac Collins>
2016-10-27 18:09:42 -0400

    Updates to personal project using alias

--- public/js/vue/components/layout/topnav-component.js
+++ public/js/vue/components/layout/topnav-component.js
@@ -21,7 +21,7 @@ Vue.component('topnav-component', {
           <iframe id="ytplayer"
               class="music-player"
               type="text/html"
-              width="500"
+              width="450"
               height="50"
               src="https://www.youtube.com/embed/?playlist=Pn-6eOxnEMI&showinfo=0"
               frameborder="0"
