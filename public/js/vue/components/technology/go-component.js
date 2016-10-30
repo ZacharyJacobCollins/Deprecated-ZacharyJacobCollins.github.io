17f051744a8e6108198ab17f70fb3ecdf589b5ad
Zac Collins <zcollins@sscorp.com>
2016-10-30 19:22:35 -0400

    Updates to personal project using alias

--- public/js/vue/components/technology/go-component.js
+++ public/js/vue/components/technology/go-component.js
@@ -18,6 +18,14 @@ Vue.component('go-component', {
         { term: "Rune", text: "Alias for int32" },
         { term: "Type Conversions", text: "The expression T(v) converts the value v to the type T." },
         { term: "Constants", text: "Constants are declared like variables, but with the const keyword. const Pi = 3.14" },
+        { term: "For loop", text: "No {} brackets required for i := 0; i < 10; i++ { sum += i }" },
+        { term: "Infinite for loop ", text: "for { fmt.print(\"infinite\") }" },
+        { term: "While loop", text: "'while' is spelled 'for' in go for sum < 1000 { sum += sum }" },
+        { term: "If statement", text: "No parens () if x < 0 { return sqrt(-x) + \"i\" } " },
+        { term: "If and else scope", text: "Variables declared inside an if short statement are also available inside any of the else blocks." },
+        { term: "switch statement", text: "automatic break in syntax, stops when case succeeds.  Ex. switch time.Saturday { case today + 0: fmt.Println(\"Today.\").  Also if no condition, same as giving it a true value" },
+        { term: "defer", text: "defers execution of a funciton until surrounding funciton returns.  neato  https://tour.golang.org/flowcontrol/12" },
+        { term: "stacking defers", text: " you can push defers onto a stack for subsequent executions https://tour.golang.org/flowcontrol/13" },
         { term: "", text: "" },
         { term: "", text: "" },
         { term: "", text: "" },
@@ -25,8 +33,7 @@ Vue.component('go-component', {
         { term: "", text: "" },
         { term: "", text: "" },
         { term: "", text: "" },
-        { term: "", text: "" },
-        { term: "Bookmark", text: "https://tour.golang.org/flowcontrol/1" },
+        { term: "Bookmark", text: "https://tour.golang.org/moretypes/1" },
       ]
     };
   }
