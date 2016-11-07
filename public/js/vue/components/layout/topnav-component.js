afd719e9982e272b2ca617c27d4c31bd6ed0b690
Zac Collins <Zac Collins>
2016-11-07 00:10:26 -0400

    Updates to personal project using alias

--- public/js/vue/components/layout/topnav-component.js
+++ public/js/vue/components/layout/topnav-component.js
@@ -16,14 +16,13 @@ Vue.component('topnav-component', {
         <div class="search nav-block">
           <!-- <input placeholder="search" type="" id="search-input" v-on:blur="reset" /> -->
 
-          Youtube Player
           <!-- Youtube player (script and settings are in youtubeplayer.js the bottom of reference.html) -->
           <iframe id="ytplayer"
               class="music-player"
               type="text/html"
               width="450"
               height="50"
-              src="https://www.youtube.com/embed/?playlist=Pn-6eOxnEMI&showinfo=0"
+              src="https://www.youtube.com/embed/?playlist=VjCAcZdTKgQ&showinfo=0"
               frameborder="0"
               allowfullscreen>
           </iframe>
