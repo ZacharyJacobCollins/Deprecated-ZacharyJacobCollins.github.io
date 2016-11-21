fe120cf2895f70edad3d6aa8579e06bc6779fb87
Zac Collins <zcollins@sscorp.com>
2016-11-20 19:22:31 -0500

    Updates to personal project using alias

--- public/js/vue/components/technology/laravel-component.js
+++ public/js/vue/components/technology/laravel-component.js
@@ -2,5 +2,29 @@ Vue.component('laravel-component', {
   template:
   `
     <h1>Laravel</h1>
+    <hr />
+    <br /><br />
+    <h3> Code Snippets</h3>
+    <hr />
+
+    <ul>
+      <template v-for="snippet in snippets">
+        <li><strong>{{ snippet.title }}</strong>: {{ snippet.code }}</li>
+      </template>
+    </ul>
   `,
+  data: function() {
+    return {
+      snippets: [
+        {
+          title: 'Compact to pass array to view',
+          code: "public function edit(Note $note) { return view('notes.edit', compact('note')); }",
+        },
+        {
+          title: 'Lab 2',
+          code: 'https://gist.github.com/ZacharyJacobCollins/c05b4dce4a85f9f46bed9d6558607139',
+        },
+      ]
+    };
+  }
 });
