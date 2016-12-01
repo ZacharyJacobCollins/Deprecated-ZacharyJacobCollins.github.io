ba5cb7d9b52a2614f72f2c8acd47b2132fabfab5
Zac Collins <zcollins@sscorp.com>
2016-12-01 10:43:42 -0500

    Updates to personal project using alias

--- public/js/vue/components/algorithms/djikstras-component.js
+++ public/js/vue/components/algorithms/djikstras-component.js
@@ -2,6 +2,7 @@ Vue.component('djikstras-component', {
   template:
   `
     <h1> Djikstra's algorthm in golang </h1>
+    <a href="http://algs4.cs.princeton.edu/41graph/">Documentation</a>
     <a href="#">Link to gist</a>
     <h3>Notable Features </h3>
     <ul>
