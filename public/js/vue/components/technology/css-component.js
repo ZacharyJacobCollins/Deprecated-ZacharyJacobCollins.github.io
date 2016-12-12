286d76557e128cdf470e807d75207cc05e71c1c7
Zac Collins <zcollins@sscorp.com>
2016-12-11 21:11:26 -0500

    text transform uppercase to css component

--- public/js/vue/components/technology/css-component.js
+++ public/js/vue/components/technology/css-component.js
@@ -13,6 +13,7 @@ Vue.component('css-component', {
       snippets: [
         { title: "Dank gradient background example: ", code: "background-image: radial-gradient(circle, #54ffe2 0%, #5c0889 70%, #000000 100%);" },
         { title: "CSS background image", code: " background: url(kiwi.svg);" },
+        { title: "Uppercase", code: "Can use a text-transform property to uppercase text with css" },
         
       ]
     };
